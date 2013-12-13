cbfd7ba14e8e94984389f66b6f91a9dc1e1e4b59
Fernando Valero <FValero@Fernandos-MacBook-Pro.local>
2013-12-13 11:56:59 -0500

    Initial Commit

---
+++ rateit!/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  rateit!
+//
+//  Created by Fernando Valero on 12/13/13.
+//  Copyright (c) 2013 centralvalero.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ rateit!/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  rateit!
+//
+//  Created by Fernando Valero on 12/13/13.
+//  Copyright (c) 2013 centralvalero.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
